a1ea3cfcfc99b911340d6940239d5b0a65eb7b8e
juniel1299 <62318700+juniel1299@users.noreply.github.com>
2024-03-19 07:48:55 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,20 @@
+# 다이나믹 프로그래밍 문제 
+# 백준 2579번 , https://www.acmicpc.net/problem/2579
+N = int(input())
+# dp에 사용할 배열과 각 계단에 저장할 값을 선언
+dp = [0] * (N+1)
+point = [0] * (N+1)
+for i in range(1, N+1):
+    point[i] = int(input())
+if N==1:
+    print(point[1])
+    exit()
+elif N==2:
+    print(sum(point[:3]))
+    exit()
+dp[1] = point[1]
+dp[2] = point[1]+point[2]
+for i in range(3, N+1):
+    dp[i] = max(dp[i-2]+point[i], dp[i-3]+point[i-1]+point[i])
+
+print(dp[-1])
